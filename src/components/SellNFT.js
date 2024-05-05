a8d4d7f13ef75d240a2678e284a7ca95791dbc2b
donkadaharsha <Donkada.H.Vardhan-1@ou.edu>
2024-05-05 15:44:16 -0500

    eslint

--- src/components/SellNFT.js
+++ src/components/SellNFT.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import { useState } from "react";
 import { uploadFileToIPFS, uploadJSONToIPFS } from "../pinata";
 import Marketplace from '../Marketplace.json';
-import { useLocation } from "react-router";
+
 
 export default function SellNFT () {
     const [formParams, updateFormParams] = useState({ name: '', description: '', price: ''});
